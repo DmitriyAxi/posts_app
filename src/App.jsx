ffaddb11e803497d1e807f0ac25c08085f05fc7c
DmitryKro <dmitry.kropachev@functor.team>
2025-03-10 09:44:58 +0300

    Code cleanup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-// import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom'; 
 import './App.css'
 import RegisterForm from './components/features/RegisterForm/RegisterForm'
